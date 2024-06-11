d40622468d6fd1daa733729ed3be852ebc8155ae
Mateo <minora.mateo@gmail.com>
2024-06-10 21:18:11 -0500

    Arreglos

--- Chat_Examen/capacitor.config.ts
+++ Chat_Examen/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Chat_examen',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "Chat_examen",
+  webDir: "dist",
 };
 
 export default config;
